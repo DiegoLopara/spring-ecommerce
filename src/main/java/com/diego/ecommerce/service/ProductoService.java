ce1cb3a8e9f96c8c3b8b777bae5b3d7208a37b76
Diego <diego.l.araque@gmail.com>
2024-08-12 20:11:36 +0200

    crear el repository y las clases y el método show de ProductController

---
+++ src/main/java/com/diego/ecommerce/service/ProductoService.java
@@ -0,0 +1,12 @@
+package com.diego.ecommerce.service;
+
+import java.util.Optional;
+
+import com.diego.ecommerce.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+}
