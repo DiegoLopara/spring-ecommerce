166f32423a652fb56b45aa83c6238aa895c313e1
Diego <diego.l.araque@gmail.com>
2024-08-14 16:46:12 +0200

    funcionalidad ver productos para el usuario (metodo home, directorio y template home.html)

--- src/main/java/com/diego/ecommerce/controller/ProductoController.java
+++ src/main/java/com/diego/ecommerce/controller/ProductoController.java
@@ -71,7 +71,7 @@ public class ProductoController {
 
 	@PostMapping("/update")
 	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
-		
+
 		Producto p = new Producto();
 		p = productoService.get(producto.getId()).get();
 		if (file.isEmpty()) {// Editamos el producto pero no cambiamos la imagen
@@ -84,7 +84,7 @@ public class ProductoController {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
-	
+
 		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
