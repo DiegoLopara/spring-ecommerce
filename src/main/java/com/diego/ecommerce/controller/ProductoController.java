b986334dee442b615b5859dbcd52eb1f781d04dd
Diego <diego.l.araque@gmail.com>
2024-08-14 15:38:46 +0200

    implementación para guardar la imagen del producto

--- src/main/java/com/diego/ecommerce/controller/ProductoController.java
+++ src/main/java/com/diego/ecommerce/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.diego.ecommerce.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.diego.ecommerce.model.Producto;
 import com.diego.ecommerce.model.Usuario;
 import com.diego.ecommerce.service.ProductoService;
+import com.diego.ecommerce.service.UploadFileService;
 
 
 
@@ -25,6 +29,9 @@ public class ProductoController {
 
 	@Autowired
 	private ProductoService productoService;
+	
+	@Autowired
+	private  UploadFileService upload;
 
 	@GetMapping("")
 	public String show(Model model) {
@@ -38,10 +45,25 @@ public class ProductoController {
 	}
 
 	@PostMapping("/save")
-	public String save(Producto producto) {
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es el objeto producto {}", producto);
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
+		
+		//imagen
+		if (producto.getId()==null) {//Cuando se crea un producto
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		} else {
+			if (file.isEmpty()) {//Editamos el producto pero no cambiamos la imagen
+				Producto p = new Producto();
+				p = productoService.get(producto.getId()).get();
+				producto.setImagen(p.getImagen());
+			}else {
+				String nombreImagen = upload.saveImage(file);
+				producto.setImagen(nombreImagen);
+			}
+		}
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
