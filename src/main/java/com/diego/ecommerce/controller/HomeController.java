11fb50dd1bd2eef2a2d30cf7931b24c6eb317f21
Diego <diego.l.araque@gmail.com>
2024-08-15 10:34:33 +0200

    mostrar información del producto en la página productohome

--- src/main/java/com/diego/ecommerce/controller/HomeController.java
+++ src/main/java/com/diego/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.diego.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,27 +10,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.diego.ecommerce.model.Producto;
 import com.diego.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
 	@Autowired
 	private ProductoService productoService;
-	
+
 	@GetMapping("")
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());
-		
+
 		return "usuario/home";
 	}
-	
+
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 		log.info("Id producto enviado como parámetro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+
 		return "usuario/productohome";
 	}
 }
