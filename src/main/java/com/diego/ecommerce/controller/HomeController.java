1764e0373f5dfb8350a59a4ea2d8d2ef098e731a
Diego <diego.l.araque@gmail.com>
2024-08-15 11:30:58 +0200

    Añadir los productos al carrito de compras y mostrarlos en la vista carrito

--- src/main/java/com/diego/ecommerce/controller/HomeController.java
+++ src/main/java/com/diego/ecommerce/controller/HomeController.java
@@ -27,11 +27,11 @@ public class HomeController {
 
 	@Autowired
 	private ProductoService productoService;
-	
-	//Para almacenar los detalles de la orden
+
+	// Para almacenar los detalles de la orden
 	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
-	
-	//Datos de la orden
+
+	// Datos de la orden
 	Orden orden = new Orden();
 
 	@GetMapping("")
@@ -47,20 +47,33 @@ public class HomeController {
 		Producto producto = new Producto();
 		Optional<Producto> productoOptional = productoService.get(id);
 		producto = productoOptional.get();
-		
+
 		model.addAttribute("producto", producto);
 
 		return "usuario/productohome";
 	}
-	
+
 	@PostMapping("/cart")
-	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
 		DetalleOrden detalleOrden = new DetalleOrden();
 		Producto producto = new Producto();
 		double sumaTotal = 0;
 		Optional<Producto> optionalProducto = productoService.get(id);
 		log.info("Producto añadido: {}", optionalProducto.get());
 		log.info("Cantidad: {}", cantidad);
+		producto = optionalProducto.get();
+		detalleOrden.setCantidad(cantidad);
+		detalleOrden.setPrecio(producto.getPrecio());
+		detalleOrden.setNombre(producto.getNombre());
+		detalleOrden.setTotal(producto.getPrecio() * cantidad);
+		detalleOrden.setProducto(producto);
+		
+		detalles.add(detalleOrden);
+		
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
 		return "usuario/carrito";
 	}
 }
