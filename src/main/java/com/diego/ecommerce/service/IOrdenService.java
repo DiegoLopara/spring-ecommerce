8068114d06788edf5a00e80dc99f7f9c5ebb6352
Diego <diego.l.araque@gmail.com>
2024-08-15 19:15:08 +0200

    Metodo para generar el número de la orden

--- src/main/java/com/diego/ecommerce/service/IOrdenService.java
+++ src/main/java/com/diego/ecommerce/service/IOrdenService.java
@@ -1,8 +1,11 @@
 package com.diego.ecommerce.service;
 
+import java.util.List;
+
 import com.diego.ecommerce.model.Orden;
 
 public interface IOrdenService {
 
+	List<Orden> findAll();
 	Orden save (Orden orden);
 }
