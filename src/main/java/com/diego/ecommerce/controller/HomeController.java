0f39f9578db06e7e8399295e5578b0dc7ad8608a
Diego <diego.l.araque@gmail.com>
2024-08-15 11:07:41 +0200

    obtener la cantidad y buscar el producto en el home controller para añadir al carrito

--- src/main/java/com/diego/ecommerce/controller/HomeController.java
+++ src/main/java/com/diego/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.diego.ecommerce.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.diego.ecommerce.model.DetalleOrden;
+import com.diego.ecommerce.model.Orden;
 import com.diego.ecommerce.model.Producto;
 import com.diego.ecommerce.service.ProductoService;
 
@@ -22,6 +27,12 @@ public class HomeController {
 
 	@Autowired
 	private ProductoService productoService;
+	
+	//Para almacenar los detalles de la orden
+	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+	
+	//Datos de la orden
+	Orden orden = new Orden();
 
 	@GetMapping("")
 	public String home(Model model) {
@@ -43,7 +54,13 @@ public class HomeController {
 	}
 	
 	@PostMapping("/cart")
-	public String addCart() {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto = new Producto();
+		double sumaTotal = 0;
+		Optional<Producto> optionalProducto = productoService.get(id);
+		log.info("Producto añadido: {}", optionalProducto.get());
+		log.info("Cantidad: {}", cantidad);
 		return "usuario/carrito";
 	}
 }
