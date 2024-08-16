cc5b794259a8b675b22437587c63be43d3629a02
Diego <diego.l.araque@gmail.com>
2024-08-16 17:01:56 +0200

    Funcionalidad ver detalle de compra al user

--- src/main/java/com/diego/ecommerce/service/IOrdenService.java
+++ src/main/java/com/diego/ecommerce/service/IOrdenService.java
@@ -1,6 +1,7 @@
 package com.diego.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.diego.ecommerce.model.Orden;
 import com.diego.ecommerce.model.Usuario;
@@ -8,6 +9,7 @@ import com.diego.ecommerce.model.Usuario;
 public interface IOrdenService {
 
 	List<Orden> findAll();
+	Optional<Orden> findById(Integer id);
 	Orden save (Orden orden);
 	String generarNumeroOrden();
 	List<Orden> findByUsuario(Usuario usuario);
