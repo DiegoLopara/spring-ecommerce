b654547aea67c400e4688ac85bc22ff4d38848c8
Diego <diego.l.araque@gmail.com>
2024-08-16 17:35:55 +0200

    Funcionalidad para ver usuarios admin

--- src/main/java/com/diego/ecommerce/service/IUsuarioService.java
+++ src/main/java/com/diego/ecommerce/service/IUsuarioService.java
@@ -1,11 +1,13 @@
 package com.diego.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.diego.ecommerce.model.Usuario;
 
 public interface IUsuarioService {
 
+	List<Usuario> findAll();
 	Optional<Usuario> findById(Integer id);
 	Usuario save (Usuario usuario);
 	Optional<Usuario> findByEmail(String email);
