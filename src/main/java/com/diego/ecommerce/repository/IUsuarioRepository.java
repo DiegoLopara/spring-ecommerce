72a55c5b3195a6383ad9726f854a6460aefa941c
Diego <diego.l.araque@gmail.com>
2024-08-16 10:52:12 +0200

    Obtener usuario login y session

--- src/main/java/com/diego/ecommerce/repository/IUsuarioRepository.java
+++ src/main/java/com/diego/ecommerce/repository/IUsuarioRepository.java
@@ -1,11 +1,16 @@
 package com.diego.ecommerce.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.diego.ecommerce.model.Usuario;
+import java.util.List;
+
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer>{
 
+	Optional<Usuario> findByEmail(String email);
 }
